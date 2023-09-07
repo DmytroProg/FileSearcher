1e2e5aafef0aa6ed5071cbc85d30376c9b589e81
DmytroProg <dmytroprog@gmail.com>
2023-09-07 18:26:51 +0300

    FileManager class added

---
+++ BusinessDataLogic/FileManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDataLogic
+{
+    internal class FileManager
+    {
+        public async Task<string> ReadFileAsync(string path)
+        {
+            using(var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using(var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+                {
+                    return await streamReader.ReadToEndAsync();
+                }
+            }
+        }
+
+        public async Task WriteFileAsync(string path, string text)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Write))
+            {
+                using (var streamWriter = new StreamWriter(fileStream, Encoding.UTF8))
+                {
+                    await streamWriter.WriteAsync(text);
+                }
+            }
+        }
+    }
+}
