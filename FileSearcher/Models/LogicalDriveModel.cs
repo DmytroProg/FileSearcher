439574f97d0e94cbc89ce34276a76889980da2f4
DmytroProg <dmytroprog@gmail.com>
2023-09-08 01:51:48 +0300

    FileSearchSettinfView designed and developed

---
+++ FileSearcher/Models/LogicalDriveModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearcher.Models
+{
+    public class LogicalDriveModel
+    {
+        public bool IsChecked { get; set; }
+        public string DriveName { get; set; } = null!;
+        public string DriveFormat { get; set; } = null!;
+    }
+}
--- FileSearcher/ViewModels/FileSearchSettingsViewModel.cs
+++ FileSearcher/ViewModels/FileSearchSettingsViewModel.cs
@@ -1,13 +1,70 @@
-﻿using FileSearcher.ViewModels.Base;
+﻿using BusinessDataLogic;
+using FileSearcher.Models;
+using FileSearcher.ViewModels.Base;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace FileSearcher.ViewModels
 {
     public class FileSearchSettingsViewModel : ViewModelBase
     {
+        private FileSearchFacade facade;
+        private string _selectedFolder;
+
+        public ObservableCollection<LogicalDriveModel> LogicalDrives { get; }
+        public string SelectedFolder {
+            get => _selectedFolder;
+            set
+            {
+                _selectedFolder = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public FileSearchSettingsViewModel()
+        {
+            facade = new FileSearchFacade();
+            LogicalDrives = new ObservableCollection<LogicalDriveModel>();
+            LoadDrives();
+        }
+
+        /// <summary>
+        /// Methods for adding information about logical drives in the collection LogicalDrives
+        /// </summary>
+        private void LoadDrives()
+        {
+            foreach (var drive in System.IO.DriveInfo.GetDrives())
+            {
+                if (drive.Name == "C:\\")
+                    continue;
+                LogicalDrives.Add(new LogicalDriveModel()
+                {
+                    DriveName = drive.Name,
+                    IsChecked = false,
+                    DriveFormat = drive.DriveFormat
+                });
+            }
+        }
+
+        public ICommand SelectFolderCommand
+        {
+            get => new RelayCommand(SelectFolder);
+        }
+
+        private void SelectFolder()
+        {
+            var folderBrowserDialog = new FolderBrowserDialog();
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                SelectedFolder = folderBrowserDialog.SelectedPath;
+            }
+        }
     }
 }
