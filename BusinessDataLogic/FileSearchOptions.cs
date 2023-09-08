57e955dd1a61a3a044fafea07be3653bfd2f18fd
DmytroProg <dmytroprog@gmail.com>
2023-09-08 22:53:23 +0300

    FileSearchOptions added

---
+++ BusinessDataLogic/FileSearchOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessDataLogic
+{
+    public class FileSearchOptions
+    {
+        public IEnumerable<string> Drives { get; set; } = null!;
+        public string SelectedFolder { get; set; } = null!;
+    }
+}
