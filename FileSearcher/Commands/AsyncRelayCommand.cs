a012544fee132b588f5846e9b5c89a1d0a9cf54e
DmytroProg <dmytroprog@gmail.com>
2023-09-08 22:53:40 +0300

    Commands added

---
+++ FileSearcher/Commands/AsyncRelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearcher.Commands
+{
+    public class AsyncRelayCommand : AsyncCommandBase
+    {
+        private readonly Func<Task> _executeAsync;
+        private readonly Func<bool> _canExecute;
+
+        public AsyncRelayCommand(Func<Task> execute) : this(execute, () => true)
+        {
+        }
+
+        public AsyncRelayCommand(Func<Task> execute, Func<bool> canExecute)
+        {
+            _executeAsync = execute;
+            _canExecute = canExecute;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            await _executeAsync.Invoke();
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _canExecute.Invoke();
+        }
+    }
+}
---
+++ FileSearcher/Commands/NavigationCommand.cs
@@ -0,0 +1,27 @@
+﻿using FileSearcher.Stores;
+using FileSearcher.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearcher.Commands
+{
+    public class NavigationCommand : CommandBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly ViewModelBase _viewModelBase;
+
+        public NavigationCommand(NavigationStore navigationStore, ViewModelBase viewModelBase)
+        {
+            _navigationStore = navigationStore;
+            _viewModelBase = viewModelBase;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            _navigationStore.CurrentViewModel = _viewModelBase;
+        }
+    }
+}
